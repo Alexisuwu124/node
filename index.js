4fd302e8b6548979d088f2adcedccc9342b0b5a7
Alexisuwu124 <55515779+Alexisuwu124@users.noreply.github.com>
2019-09-26 22:58:18 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var app = express();
+var datos=[ ];
+var persona={};
 
-app.get('/', function (req, res) {
+
+
+app.get("/", (req, res) =>{
+	console.log(req);
   res.send('OwO');
 });
 
-app.listen(3000, function () {
+app.get("/:id/:persona/:edad", (req, res) =>{
+	if(req.url!="favicon.ico"){
+		var dato=req.params.id;
+		var dato1=req.params.persona;
+		var dato2=req.params.edad;
+
+		console.log(dato);
+		console.log(dato1)
+		console.log(dato2);
+		datos.push(dato);
+		persona.nombre=dato1;
+		persona.edad=dato2;
+		res.send('Si se pudo');
+		console.log(datos);
+		console.log(persona);
+	};
+	
+app.listen(3000, () =>{
   console.log('El servidor esta corriendo...');
 });
